1468ff3ed8b995c43b57bd130ccdc15f2de60ae8
Robert Bentall <robertbentall@gmail.com>
2024-02-19 09:31:06 +0000

    Sort out Milestones to include color and commit date. Handles null values of commit date.

--- src/main/java/com/rabentall/processdash_reporting/Milestones.java
+++ src/main/java/com/rabentall/processdash_reporting/Milestones.java
@@ -8,7 +8,35 @@ import net.sourceforge.processdash.api.PDashContext;
 
 class Milestones extends DashDataList{
 
+  transient String MILESTONE_COLOR_HQL =
+  " select                                 " +
+  "   piaf.planItem.id,                    " +
+  "   piaf.value.text                      " +
+  " from                                   " +
+  "   PlanItemAttrFact as piaf             " +
+  " where                                  " +
+  "   piaf.attribute.name='Milestone Color'";
+
+  transient Lookup<String> milestoneColors_ = new Lookup<String>(MILESTONE_COLOR_HQL);
+
+  transient String MILESTONE_COMMT_DATE_HQL =
+  " select                                       " +
+  "   piaf.planItem.id,                          " +
+  "   piaf.value.text                            " +
+  " from                                         " +
+  "   PlanItemAttrFact as piaf                   " +
+  " where                                        " +
+  "   piaf.attribute.name='Milestone Commit Date'";
+
+  transient Lookup<String> milestoneCommitDate_ = new Lookup<String>(MILESTONE_COMMT_DATE_HQL);
+
+
   void load(PDashContext ctx) {
+
+    description = "Milestone information for each planItem.";
+
+    milestoneColors_.load(ctx);
+    milestoneCommitDate_.load(ctx);
 
     String hql =
     " select                                                         " +
@@ -16,17 +44,14 @@ class Milestones extends DashDataList{
     "   dep.predecessor.project.id,                                  " +
     "   dep.predecessor.wbsElement.id,                               " +
     "   dep.predecessor.task.id,                                     " +
-    "   dep.successor.task.name,                                     " +
-    "   piaf.value.text,                                              " +
-    "   dep.predecessor.project.name,                   " +
-    "   dep.predecessor.wbsElement.name,                " +
-    "   dep.predecessor.task.name                       " +
-    " from PlanItemAttrFact as piaf, PlanItemDependencyFact as dep   " +
+    "   dep.predecessor,                                             " +
+    "   dep.successor.id,                                            " +
+    "   dep.successor,                                               " +
+    "   dep.successor.task.name                                      " +
+    " from PlanItemDependencyFact as dep                             " +
     " where                                                          " +
-    " dep.successor.id = piaf.planItem.id and                        " +
-    " dep.successor.phase.name = 'Milestone' and                     " +
-    " piaf.planItem.phase.identifier = '*Unspecified*/Milestone' and " +
-    " piaf.attribute.name='Milestone Commit Date'                    ";
+    "   dep.successor.phase.name = 'Milestone' and                   " +
+    "   dep.successor.phase.identifier = '*Unspecified*/Milestone'   ";
 
     load(ctx, hql);
   }
@@ -34,28 +59,40 @@ class Milestones extends DashDataList{
   void addElement(Object[] row) throws ParseException{
     elements.add(new Milestone(row));
   }
-}
 
-class Milestone extends DashDataElement{
+  class Milestone extends DashDataElement{
 
-  Integer planItemId;
-  Integer projectId;
-  Integer wbsElementId;
-  Integer taskId;
-  String  name;
-  Date    commitDate;
-  String  planItem;
+    Integer planItemId;
+    Integer projectId;
+    Integer wbsElementId;
+    Integer taskId;
+    String  planItem;
+    Integer milestonePlanItemId;
+    String  milestonePlanItem;
+    String  milestoneName;
 
-  Milestone(Object[] row) throws ParseException{
-    planItemId   = (Integer)row[0];
-    projectId    = (Integer)row[1];
-    wbsElementId = (Integer)row[2];
-    taskId       = (Integer)row[3];
-    name         = (String) row[4];
-    planItem     = (String) row[6] + "/" + (String) row[7] + "/" + (String) row[8];
+    Date    commitDate;
+    String  milestoneColor;
 
-    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-    commitDate    = (sdf.parse((String)row[5]));
+    Milestone(Object[] row) throws ParseException{
+      planItemId          = (Integer)row[0];
+      projectId           = (Integer)row[1];
+      wbsElementId        = (Integer)row[2];
+      taskId              = (Integer)row[3];
+      planItem            = getNullablePlanItemString(row[4]);
+      milestonePlanItemId = (Integer)row[5];
+      milestonePlanItem   = getNullablePlanItemString(row[6]);
+      milestoneName       = (String)row[7];
+
+      milestoneColor      = milestoneColors_.elements.get(milestonePlanItemId);
+
+      String milestoneCommitDateStr = milestoneCommitDate_.elements.get(milestonePlanItemId);
+
+      //FIXME - UGLY. Also not sure the output is more useful than yyyy-mm-dd/
+      //Dec 31, 2023 12:00:00 AM
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+      commitDate    = sdf.parse(milestoneCommitDateStr);
+    }
+    Milestone(){}
   }
-  Milestone(){}
 }
